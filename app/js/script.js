76f6f445885e9721163a9b175c7302d7773c942c
TiredQuan <85377764+TiredQuan@users.noreply.github.com>
2021-11-18 08:42:29 +0700

    nearly finished nav

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,14 @@
-let mobileMenu=document.getElementById('mobile-menu')
 let menuBruh=document.getElementById('menu-x')
+let mobileMenu=document.getElementById('mobile-menu')
 const menuX=()=>{
     if(menuBruh.classList.contains('fa-bars')){
         menuBruh.classList.remove('fa-bars')
         menuBruh.classList.add('fa-times')
+        mobileMenu.classList.add('show')
     } else {
         menuBruh.classList.remove('fa-times')
         menuBruh.classList.add('fa-bars')
+        mobileMenu.classList.remove('show')
 
     }
 }
